fe817086cc8c35776c15a5fa5f04c17d341273b0
Danilo LM <danilolopesdemoraes@gmail.com>
2021-06-07 10:20:48 -0300

    feat: api-docs swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response, NextFunction } from "express";
+import swaggerUi from "swagger-ui-express";
 
 import "express-async-errors";
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 import { CustomError } from "./utils/CustomError";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -12,11 +12,12 @@ class ListAllUsersUseCase {
   execute({ user_id }: IRequest): User[] {
     const user = this.usersRepository.findById(user_id);
 
-    if (user_id && !user) {
+    if (user_id !== "undefined" && user_id != null && !user) {
+      console.log(user_id);
       throw new CustomError("User not existis!!", 400);
     }
 
-    if (user_id && user.admin === false) {
+    if (user_id !== "undefined" && user.admin === false) {
       throw new CustomError("User is not admin!!", 400);
     }
 
